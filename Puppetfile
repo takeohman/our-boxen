53e9859002b539ded86ef32883cc8b7493583e8e
Takeo Hidaka <takeoh@takeo-no-mbp.lan>
2014-01-14 12:39:04 +0900

    customizing for me.

--- Puppetfile
+++ Puppetfile
@@ -55,3 +55,24 @@ github "xquartz",    "1.1.0"
 
 # Optional/custom modules. There are tons available at
 # https://github.com/boxen.
+
+github "osx",            "1.6.0"
+
+github "java",           "1.1.0"
+github "php",            "1.1.0"
+github "libtool",        "1.0.0" # use for php
+github "pkgconfig",      "1.0.0" # use for php
+github "pcre",           "1.0.0" # use for php
+github "libpng",         "1.0.0" # use for php
+github "imagemagick",	"1.2.0"
+
+github "mysql",		"1.2.0"
+github "wget",  	"1.0.0"
+
+github "chrome",	"1.1.2"
+github "firefox",	"1.1.1"
+github "skype",		"1.0.8"
+github "mou",		"1.1.3"
+github "textwrangler",	"1.0.2"
+github "libreoffice",	"4.1.4"
+github "virtualbox",	"1.0.5"
---
+++ modules/people/manifests/takeohman.pp
@@ -0,0 +1,77 @@
+class people::takeohman {
+	## osx
+	# Finder
+	include osx::finder::unhide_library
+	class osx::finder::show_all_files {
+		include osx::finder
+		boxen::osx_defaults { 'Show all files':
+			user   => $::boxen_user,
+			domain => 'com.apple.finder',
+			key    => 'AppleShowAllFiles',
+			value  => true,
+			notify => Exec['killall Finder'];
+		}
+	}
+	include osx::finder::show_all_files
+
+	# Dock
+	include osx::dock::autohide
+
+	class osx::dock::kill_dashbord{
+		include osx::dock
+		boxen::osx_defaults { 'kill dashbord':
+			user   => $::boxen_user,
+			domain => 'com.apple.dashboard',
+			key    => 'mcx-disabled',
+			value  => YES,
+			notify => Exec['killall Dock'];
+		}
+	}
+	include osx::dock::kill_dashbord
+
+	# Universal Access
+	include osx::universal_access::ctrl_mod_zoom
+	include osx::universal_access::enable_scrollwheel_zoom
+
+	# Miscellaneous
+	include osx::no_network_dsstores # disable creation of .DS_Store files on network shares
+	include osx::software_update # download and install software updates
+
+	# include turn-off-dashboard
+
+	# lib
+	include java
+	include php::5_4
+	include mysql
+
+	include chrome::chromium
+	include wget
+	include textwrangler	
+	include skype
+
+	include libreoffice
+
+	## or set your own locale
+	class { 'libreoffice::languagepack':
+	  locale => 'ja'
+	}
+
+	package {
+	'PhpStorm':
+	source=>'http://download-ln.jetbrains.com/webide/PhpStorm-7.1.dmg',
+	provider=>'appdmg';
+
+	'FileZilla':
+	source=>'http://jaist.dl.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_i686-apple-darwin9.app.tar.bz2',
+	provider=>'compressed_app';
+
+	'GoogleJapaneseInput':
+	source=>'https://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg',
+	provider=>'pkgdmg';
+
+	'SourceTree':
+	source=>'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.0.3.dmg',
+	provider=>'appdmg';
+	}
+}
+
